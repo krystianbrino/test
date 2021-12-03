41285441978d10a2ce998fa411c9468f39bbcaeb
Krystian Knott <knocik5@gmail.com>
2021-12-03 23:50:35 +0100

    jej

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const podajImie = prompt("Podaj imie");
+var age = parseInt(prompt("How old are you?"));
 
-alert(podajImie + " " + podajImie.length);
+console.log("Only +18 can access the website");
+
+if(age >= 18) {
+    alert("Welcome to the website");
+}
+else {
+    alert("You're not allowed to access to the website");
+}
